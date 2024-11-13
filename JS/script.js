d3be6b5ec4dc48b29863cdd1a9474774b28b2616
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-11-13 14:26:31 +0100

    add(Create algorithms of creation of gift boxes)

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
 console.log("Hello");
+
+const boxAdventCalendar = document.getElementById('box');
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function createGifts() {
+    var giftsList = [];
+    for (let index = 0; index < 24; index++) {
+        var giftBox = document.createElement('div');
+        var texteGift = document.createElement('p');
+        texteGift.textContent = index + 1;
+        giftBox.appendChild(texteGift);
+        giftsList.push(giftBox);
+    }
+    shuffle(giftsList);  // Mélanger la liste des cadeaux
+
+    // Ajouter les cadeaux mélangés au calendrier de l'avent
+    giftsList.forEach(gift => boxAdventCalendar.appendChild(gift));
+}
+
+createGifts();
