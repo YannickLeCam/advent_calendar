6ac10e8d6f48ffac19b476746f51b806ca1e6fda
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-11-15 12:38:38 +0100

    add(Class to make a difference to detect sundays)

--- JS/script.js
+++ JS/script.js
@@ -12,8 +12,17 @@ function shuffle(array) {
 
 function createGifts() {
     var giftsList = [];
+    var bigGiftIndex = 0;
     for (let index = 0; index < 24; index++) {
         var giftBox = document.createElement('div');
+        giftBox.classList.add('giftBox');
+        if (index===bigGiftIndex) {
+            giftBox.classList.add('bigGiftBox');
+            bigGiftIndex+=7;
+        }
+        if (index === 23) {
+            giftBox.classList.add('lastGiftBox');
+        }
         var texteGift = document.createElement('p');
         texteGift.textContent = index + 1;
         giftBox.appendChild(texteGift);
